a711d1cfb3903d41830eb7a28c90761b5b16dfac
Vivek Maurya <Vivek563maurya@gmail.com>
2024-03-26 21:34:52 +0530

    feat: optimized the floating windows

--- src/windows.tsx
+++ src/windows.tsx
@@ -3,111 +3,113 @@ import React, { useEffect, useRef, useState } from "react";
 import { FiMaximize2, FiMinus } from "react-icons/fi";
 import { IoIosClose } from "react-icons/io";
 
-interface WindowProps {
+interface FloatingWindowProps {
      title: string;
      children: React.ReactNode;
 }
 
-const Window: React.FC<WindowProps> = ({ title, children }) => {
+const FloatingWindow: React.FC<FloatingWindowProps> = ({ title, children }) => {
      const [isMinimized, setIsMinimized] = useState(false);
      const [windowSize, setWindowSize] = useState({
-          width: window.innerWidth,
-          height: window.innerHeight,
+          width: 0, // Initial width set to 0
+          height: 0, // Initial height set to 0
      });
-     const windowRef = useRef<HTMLDivElement>(null); // Ref for window div
-     const titleBarRef = useRef<HTMLDivElement>(null); // Ref for title bar div
-
-     const handleResize = () => {
-          setWindowSize({
-               width: window.innerWidth,
-               height: window.innerHeight,
-          });
-     };
+     const windowRef = useRef<HTMLDivElement>(null);
+     const titleBarRef = useRef<HTMLDivElement>(null);
+     const [dragConstraints, setDragConstraints] = useState<DraggableProps["dragConstraints"]>({
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+     });
 
      useEffect(() => {
-          window.addEventListener("resize", handleResize);
-
-          return () => {
-               window.removeEventListener("resize", handleResize);
+          const handleResize = () => {
+               setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+               });
           };
-     }, []);
 
-     useEffect(() => {
-          // Function to update constraints based on window size
           const updateConstraints = () => {
                if (windowRef.current && titleBarRef.current) {
                     const titleBarWidth = titleBarRef.current.offsetWidth;
-                    const h = windowSize.height - 50;
-                    const r = windowSize.width - titleBarWidth - 4;
+                    const height = windowSize.height - 50;
+                    const right = windowSize.width - titleBarWidth - 4;
                     setDragConstraints({
                          top: 0,
                          left: 0,
-                         bottom: h ?? 0,
-                         right: r,
+                         bottom: height ?? 0,
+                         right: right,
                     });
                }
           };
 
-          updateConstraints(); // Call initially
-          window.addEventListener("resize", updateConstraints); // Listen for resize events
+          const resizeObserver = new ResizeObserver(() => {
+               handleResize();
+               updateConstraints();
+          });
+
+          if (windowRef.current) {
+               resizeObserver.observe(windowRef.current);
+          }
+          if (titleBarRef.current) {
+               resizeObserver.observe(titleBarRef.current);
+          }
 
           return () => {
-               window.removeEventListener("resize", updateConstraints);
+               resizeObserver.disconnect();
           };
-     }, [windowSize, isMinimized]);
-
-     const [dragConstraints, setDragConstraints] = useState<DraggableProps["dragConstraints"]>({
-          top: 0,
-          left: 0,
-          bottom: windowSize.height - 50,
-          right: windowSize.width - 320, // Initial value, will be updated dynamically
-     });
+     }, [windowSize]);
 
      const handleClose = () => {
           // Implement close functionality
      };
 
-     const handleMinimize = () => {
-          setIsMinimized(!isMinimized);
+     const handleToggleMaximize = () => {
+          setIsMinimized(false);
+     };
+
+     const handleToggleMinimize = () => {
+          setIsMinimized(true);
      };
 
      return (
-          <>
-               <div
-                    style={{
-                         width: "100vw",
-                         height: "100vh",
-                         position: "relative",
-                    }}
+          <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
+               {/* Title bar */}
+               <motion.div
+                    ref={windowRef}
+                    drag
+                    dragConstraints={dragConstraints}
+                    dragTransition={{ power: 0 }}
+                    dragElastic={0.3}
+                    className={`absolute rounded-xl w-auto h-auto  text-center  bg-[#BBBBBB] ${isMinimized ? "minimized" : ""}`}
                >
-                    {/*........Title bar........*/}
-                    <motion.div
-                         ref={windowRef}
-                         drag
-                         dragConstraints={dragConstraints}
-                         dragTransition={{ bounceStiffness: 800, bounceDamping: 25 }}
-                         className={`absolute rounded-xl w-auto h-auto border-2 text-center border-white bg-[#BBBBBB] ${isMinimized ? "minimized" : ""}`}
-                    >
-                         <div ref={titleBarRef} className={`w-full bg-[#ffffff] flex justify-start pr-4  gap-[0.10rem] ${isMinimized ? "rounded-lg" : "rounded-t-lg"}  `}>
-                              <div className="cursor-pointer gap-1 flex p-[0.8rem] ">
-                                   <button className=" w-[20px] h-[20px] flex justify-center items-center bg-[#FE5D55] rounded-full" onClick={handleClose}>
-                                        <IoIosClose size={"100%"} />
+                    <div ref={titleBarRef} className={`w-full bg-[#ffffff] flex justify-start pr-4  gap-[0.10rem] ${isMinimized ? "rounded-lg" : "rounded-t-lg"}  `}>
+                         <div className="cursor-pointer gap-1 flex p-[0.8rem] ">
+                              <button className=" w-[20px] h-[20px] flex justify-center items-center bg-[#FE5D55] rounded-full" onClick={handleClose}>
+                                   <IoIosClose size={"100%"} />
+                              </button>
+                              {isMinimized ? (
+                                   <button className={`w-[20px] h-[20px] flex justify-center items-center bg-[#FBC22F] rounded-full p-1`} onClick={handleToggleMaximize}>
+                                        <FiMaximize2 size={"100%"} />
                                    </button>
-                                   <button className={`w-[20px] h-[20px] flex justify-center items-center ${isMinimized ? "bg-[#01CE4B]" : "bg-[#FBC22F]"}  rounded-full p-1`} onClick={handleMinimize}>
-                                        {isMinimized ? <FiMaximize2 size={"100%"} /> : <FiMinus size={15} />}
+                              ) : (
+                                   <button className={`w-[20px] h-[20px] flex justify-center items-center bg-[#FBC22F] rounded-full p-1`} onClick={handleToggleMinimize}>
+                                        <FiMinus size={15} />
                                    </button>
-                              </div>
-                              <div className="flex items-center justify-center text-center">{title}</div>
+                              )}
                          </div>
+                         <div className="flex items-center justify-center text-center">{title}</div>
+                    </div>
 
-                         {!isMinimized && (
-                              //...content height....
-                              <div className={`bg-[#ffffff]  m-2 rounded-xl p-3 flex items-center justify-center`}>{children}</div>
-                         )}
-                    </motion.div>
-               </div>
-          </>
+                    {!isMinimized && (
+                         // Content height
+                         <div className={`bg-[#ffffff]  m-2 rounded-xl p-3 flex items-center justify-center`}>{children}</div>
+                    )}
+               </motion.div>
+          </div>
      );
 };
 
-export default Window;
+export default FloatingWindow;
